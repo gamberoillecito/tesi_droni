0e365b6e136b98339ce24ee60b56e2812ea30914
gamberoillecito <itjustfordrive@gmail.com>
2023-04-18 19:41:46 +0200

    aggiunto download appimage

--- install_qgroundcontrol.sh
+++ install_qgroundcontrol.sh
@@ -1,10 +1,12 @@
 # comandi presi da https://docs.qgroundcontrol.com/master/en/getting_started/download_and_install.html
-sudo usermod -a -G dialout $USER
-sudo apt-get remove modemmanager -y
-sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
-sudo apt install libqt5gui5 -y
-sudo apt install libfuse2 -y
+#sudo usermod -a -G dialout $USER
+#sudo apt-get remove modemmanager -y
+#sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
+#sudo apt install libqt5gui5 -y
+#sudo apt install libfuse2 -y
 
+wget https://d176tv9ibo4jno.cloudfront.net/latest/QGroundControl.AppImage
+chmod +x ./QGroundControl.AppImage
 # questo comando e' suggerito da questo commento (solo per installazione tramite WSL2)
 # https://docs.qgroundcontrol.com/master/en/getting_started/download_and_install.html
-sudo apt-get install -y libpulse-dev
+#sudo apt-get install -y libpulse-dev
