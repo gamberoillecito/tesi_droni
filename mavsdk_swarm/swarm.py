eb37e17b52ae88cceb387ea2d0504681ac5bc916
gamberoillecito <itjustfordrive@gmail.com>
2023-07-08 18:28:27 +0200

    primi test funzionanti sul controllo dei droni

--- mavsdk_swarm/swarm.py
+++ mavsdk_swarm/swarm.py
@@ -1,8 +1,10 @@
 from loguru import logger
+from pprint import pprint
 from mavsdk import System
 import asyncio
 from typing import List, Callable
 from systemwrapper import SystemWrapper
+from droneposition import DronePosition
 
 
 class Swarm:
@@ -17,6 +19,7 @@ class Swarm:
         Arguments:
             drones_number -- Numero di droni da cui è composta la flotta
 
+        pass
         Keyword Arguments:
             drones_addrs -- Indirizzi dei droni (default: {None})
 
@@ -26,6 +29,7 @@ class Swarm:
         '''                
         self.drones_number = drones_number
         self.__positions = []
+        self.drones:List[System] = []
         # se non viene passata una lista di indirizzi vengono usati quelli di 
         # default a partire da next_drone_address
         if drones_addrs == None:
@@ -41,7 +45,6 @@ class Swarm:
 
 
     async def connect(self):
-        self.drones:List[System] = []
         logger.info("Connecting to drones...")
         for a in self.drones_addrs:
             logger.info(f"Connecting to drone at {a}")
@@ -57,38 +60,53 @@ class Swarm:
             await d.action.takeoff()
 
     async def land(self):
-        #TODO controllare se i droni sono connessi 
         for d in self.drones:
             await d.action.land()
 
     @property
-    async def positions(self):
-        p = await anext(self.drones[0].telemetry.position())
-        print(p)
-        # try:
-        #     async for pos in self.drones[0].telemetry.position():
-        #         print(pos)
-        # except asyncio.CancelledError:
-        #     return 0
-        # self.__positions = []
-        # # for n,d in enumerate(self.drones):
-        # #     self.__positions.append(d.telemetry.position())
-        # # return self.__positions
+    async def positions(self) -> List[List[DronePosition]]:
+        self.__positions = []
+        for d in self.drones:
+            p = await anext(d.telemetry.position())
+            pos = DronePosition.from_mavsdk_position(p)
+            self.__positions.append(pos)
 
+        return self.__positions
+    
+    # Non si possono fare i setter asincroni
+    # @positions.setter
+    # async def positions(self, target_positions:List[List[float]]):
+    #     for n, d in enumerate(self.drones):
+    #         pos = self.positions[n]
+    #         await d.action.goto_location(pos.latitude_deg, pos.longitude_deg, pos.absolute_altitude_m + 1, 0)
+    # async def do_for_all(self, function:Callable):
+    #     for d in self.drones:
+    #         function(d)
+    async def set_positions(self, target_positions:List[List[float]]):
+        positions = await self.positions
+        for n, d in enumerate(self.drones):
+            pos = positions[n]
+            logger.debug("vado in posizione")
+            await d.action.goto_location(pos.latitude_deg+1, pos.longitude_deg+1, pos.absolute_altitude_m + 1, 0)
     async def do_for_all(self, function:Callable):
         for d in self.drones:
             function(d)
 
+def print_pos(pos):
+    for p in pos:
+        print(p)
+    print("#"*10)
+
 async def main():
     sw = Swarm(2)
     await sw.connect()
-    # for p in sw.positions:
-    #     async for i in p:
-    #         print(p)
-    await sw.positions
     await sw.takeoff()
+    print_pos(await sw.positions)
+    await asyncio.sleep(10)
+    await sw.set_positions([[2.0], [1.0]])
     await asyncio.sleep(10)
     await sw.land()
+    print_pos(await sw.positions)
 
 if __name__ == "__main__":
     asyncio.run(main())
